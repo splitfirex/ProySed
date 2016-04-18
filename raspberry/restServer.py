41630ca977a72e5701e20b926465dbc99c9da113
Daniel Fariña Fernández <Daniel Faria Fernndez>
2016-04-18 23:27:39 +0200

    Se incluye el servicio rest y los cambios para la utilizacion de material design

---
+++ raspberry/restServer.py
@@ -0,0 +1,62 @@
+import web
+import xml.etree.ElementTree as ET
+import datetime
+from xml.etree.ElementTree import Element
+
+urls = (
+    '/consultar/historico', 'list_alarma',
+    '/consultar/estado', 'get_state',
+    '/insertar/movimiento', 'insert_alarma',
+    '/cambiar/estado/(.*)', 'update_state'
+)
+
+app = web.application(urls, globals())
+
+class list_alarma:
+	def GET(self):
+		tree = ET.parse('alarm_data.xml')
+		root = tree.getroot()
+		output = 'data: ['
+		for nodo in root:
+			output += str(nodo.attrib) + ','
+		output += ']';
+		return output
+
+class get_state:
+	def GET(self):
+		tree = ET.parse('alarm_data.xml')
+		root = tree.getroot()
+		state = root.get('estado')
+		state_since = root.get('estado-desde')
+		output = 'data: {'
+		output += 'estado: "{0}" , since: "{1}"'.format(state, state_since)
+		output += '}'
+		return output
+
+class insert_alarma:        
+    def GET(self):
+    	tree = ET.parse('alarm_data.xml')
+    	root = tree.getroot()    	
+    	raw_data = web.input()
+    	e = Element('movimiento')
+    	if raw_data:
+	    	e.set('tipo',raw_data.tipo)
+	    	e.set('nombre', raw_data.nombre)
+	    	e.set('fecha',raw_data.fecha)
+	    	if raw_data.imagen is not None:
+	    		e.set('imagen',raw_data.imagen)
+	    	root.insert(0,e)
+	        tree.write('user_data.xml', xml_declaration=False)
+	        return 'OK'
+
+class update_state:
+    def GET(self, state):
+    	tree = ET.parse('alarm_data.xml')
+    	root = tree.getroot()
+    	root.set('estado',state)
+    	root.set('estado-desde', datetime.datetime.now().strftime("%Y/%B/%d %I:%M:%S.%p"))
+    	tree.write('alarm_data.xml', xml_declaration=False)
+    	return 'OK'
+    
+if __name__ == "__main__":
+    app.run()
