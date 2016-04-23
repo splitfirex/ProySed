dcbb4d8acd4bbf522f831ed4a0d3acda9c8fd4dd
Daniel Fariña Fernández <Daniel Faria Fernndez>
2016-04-23 19:26:49 +0200

    update de script

--- start_servers.sh
+++ start_servers.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-cd webserver &&
-python -m SimpleHTTPServer 8080 && 
-cd .. && cd raspberry && 
-python motion_detector.py && python restServer.py 8081
+cd webserver & python -m SimpleHTTPServer 8080 & cd .. & cd raspberry & python motion_detector.py & python restServer.py 8081
